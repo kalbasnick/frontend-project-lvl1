403d28d7167dde6dad74f8c710a12ac4a7eaf1e8
kalbasnick <64909627+kalbasnick@users.noreply.github.com>
2020-05-15 12:54:52 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,5 @@ import readlineSync from 'readline-sync';
 
 const userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
 console.log(`Hello ${userName}!`);
-ff
+
 export default userName;
