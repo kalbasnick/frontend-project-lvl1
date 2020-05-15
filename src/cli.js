f3497f93962bb77fc072e247f4ed67125495ffa1
Kirichenko Nikita <knick@MacBook-Pro-Kirichenko.local>
2020-05-15 12:51:34 +0300

    test linter

--- src/cli.js
+++ src/cli.js
@@ -2,5 +2,6 @@ import readlineSync from 'readline-sync';
 
 const userName = readlineSync.question('Welcome to the Brain Games! \nMay I have your name? ');
 console.log(`Hello ${userName}!`);
+ff
 
 export default userName;
