5d2485185a9273739c7b6f011ba8614bb720e622
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-04-18 12:03:51 +0300

    second step of project

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import askQuestion from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
+const userName = askQuestion('May I have your name? ');
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,5 @@
+import readlineSync from 'readline-sync';
+
+const askQuestion = (question) => readlineSync.question(question);
+
+export default askQuestion;
