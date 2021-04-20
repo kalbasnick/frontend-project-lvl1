bdc8a09399f4f4c5d7a6f6430bf56ec9df316638
Kirichenko Nikita <knick@MacBook-Pro.local>
2021-04-20 18:00:10 +0300

    fixed brain-calc

--- games/calc.js
+++ games/calc.js
@@ -6,7 +6,7 @@ import askQuestion from '../src/cli.js';
 const runRoundCalculate = () => {
   const [num1, num2] = [getRandomInt(1, 100), getRandomInt(1, 100)];
   const operator = getRandomMathOperator();
-  console.log(`What is the result of the expression?\nQuestion: ${num1}${operator}${num2}`);
+  console.log(`What is the result of the expression?\nQuestion: ${num1} ${operator} ${num2}`);
   const userAnswer = askQuestion('Your answer: ');
   const correctAnswer = countExpression(num1, num2, operator);
 
